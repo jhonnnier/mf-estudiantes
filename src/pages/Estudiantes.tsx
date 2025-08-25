d6a93a77dabcd41434367cb96cddf7612b376d60
Andrés Torres <ctorres437@hotmail.com>
2025-08-24 20:02:41 -0500

    feat: añadida fetch

--- src/pages/Estudiantes.tsx
+++ src/pages/Estudiantes.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+
+
 export default function Estudiantes(props) {
+    
+    const [estudiantes, setEstudiantes] = useState("NA")
+
+    useEffect(() => {
+        fetch("http://localhost:5001/estudiantes")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("Respuesta de /estudiantes:", data.estudiantes);
+                setEstudiantes(data.estudiantes)
+            })
+            .catch((error) => {
+                console.error("Error en la petición /estudiantes:", error);
+            });
+    }, []);
     return (
         <div>
-            <h3>Estudiantes</h3>
+            <h3>{estudiantes}</h3>
         </div>
     );
 }
